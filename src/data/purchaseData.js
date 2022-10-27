6e7a4176462a0d8963b09e967b24c8e2f1bafd89
abdubosit-js <abdubositjs@gmail.com>
2022-10-27 18:39:14 +0500

    ozgardi

--- src/data/purchaseData.js
+++ src/data/purchaseData.js
@@ -13,7 +13,7 @@ export const purchaseData = {
             id: 1,
             lbs: 20,
             price: 30.15,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_3: [
@@ -21,7 +21,7 @@ export const purchaseData = {
             id: 1,
             lbs: 30,
             price: 42.25,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_4: [
@@ -29,7 +29,7 @@ export const purchaseData = {
             id: 1,
             lbs: 40,
             price: 52.52,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_5: [
@@ -37,7 +37,7 @@ export const purchaseData = {
             id: 1,
             lbs: 50,
             price: 65.55,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_6: [
@@ -45,7 +45,7 @@ export const purchaseData = {
             id: 1,
             lbs: 60,
             price: 78.50,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_7: [
@@ -53,7 +53,7 @@ export const purchaseData = {
             id: 1,
             lbs: 70,
             price: 91.11,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
     cat_8: [
@@ -61,7 +61,7 @@ export const purchaseData = {
             id: 1,
             lbs: 80,
             price: 107.65,
-            title: "YOU’RE QUALIFIED FOR FREE SHIPPING"
+            title: "YOU'RE QUALIFIED FOR FREE SHIPPING"
         }
     ],
 }
